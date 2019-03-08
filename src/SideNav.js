7078da4f907d623bef0f6c5ac01c8291345aa546
averyevans <averyevans@bellsouth.net>
2019-03-07 19:50:47 -0500

    added links and routes to 'Today', 'Three Day', and 'Ten Day'

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
+import { Link } from 'react-router-dom'
+
+// components!
+import Today from './Today';
+import ThreeDay from './ThreeDay';
+import TenDay from './TenDay';
+
 
 class SideNav extends Component {
-
-     state = {
-
-     }
-
-
 
 
      render () {
           return (
-               <div className="nav">
-                    
-               </div>
+
+                    <div className="nav">
+                         <Link to="/" id="first-link" class="link">Today</Link>
+                         <Link to="/threeday" class="link">3 Day</Link>
+                         <Link to="/tenday" id="last-link" class="link">7 Day</Link>
+                    </div>
+
           );
      }
 
