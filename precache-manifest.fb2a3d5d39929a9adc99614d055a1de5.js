871111f00cd1b76bd75f933e6f19fd2fb5db94f4
averyevans <averyevans@bellsouth.net>
2019-03-28 18:52:43 -0400

    Updates

---
+++ precache-manifest.fb2a3d5d39929a9adc99614d055a1de5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "a04865973f14992de31b",
+    "url": "/weather-app/static/js/main.a0486597.chunk.js"
+  },
+  {
+    "revision": "decdb548c95165c02d58",
+    "url": "/weather-app/static/js/2.decdb548.chunk.js"
+  },
+  {
+    "revision": "a04865973f14992de31b",
+    "url": "/weather-app/static/css/main.348dc12d.chunk.css"
+  },
+  {
+    "revision": "e1d1b15ccce2fbb3b748453d9784fbbf",
+    "url": "/weather-app/index.html"
+  }
+];
